efe5f8b86cb086e69456d51c2bd97364752d9565
Dovmial <nma91@mail.ru>
2025-03-15 12:41:11 +0500

    dz3 SquareCalculate

---
+++ SquareEquation/SquareEquationHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace SquareEquation
+{
+    public class SquareEquationHandler
+    {
+        public static double[] Solve(double a, double b, double c, double epsilon = 0.00000001)
+        {
+            if (Math.Abs(a) <= epsilon)
+                throw new Exception("A mustnot be zero!");
+            double D = b * b - 4.0 * a * c;
+            double[] roots = new double[2];
+            if (D < 0)
+                return [];
+            else if(D > epsilon)
+            {
+                roots[0] = (-b + Math.Sqrt(D)) / (2 * a);
+                roots[1] = (-b - Math.Sqrt(D)) / (2 * a);
+            }
+            else if(Math.Abs(D) <= epsilon)
+            {
+                roots[0] = roots[1] = -b / (2 * a);
+            }
+            return roots;
+        }
+    }
+}
---
+++ TestSquareEquation/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using SquareEquation;
+
+namespace TestSquareEquation
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(1.0, 0, 1.0)]
+        public void TestResolveRootsNone(double a, double b, double c)
+        {
+            var result = SquareEquationHandler.Solve(a, b, c);
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(1.0, 0, -1.0)]
+        public void TestResolveRootsTwo(double a, double b, double c)
+        {
+            double[] result = SquareEquationHandler.Solve(a, b, c);
+            Assert.Contains(1.0, result);
+            Assert.Contains(-1.0, result);
+        }
+
+        [Theory]
+        [InlineData(1.0,2.0,1.0)]
+        public void TestResolveRootsOne(double a, double b, double c)
+        {
+            double[] result = SquareEquationHandler.Solve(a, b, c);
+            Assert.Contains(-1.0, result);
+        }
+
+        [Theory]
+        [InlineData(0, 1.0, 1.0)]
+        public void TestResolveANotZero(double a, double b, double c)
+        {
+            Assert.Throws<Exception>(() => SquareEquationHandler.Solve(a,b,c));
+        }
+    }
+}
