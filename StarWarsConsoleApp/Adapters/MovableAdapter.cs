c5e7a3a0b5fccca24e02835f32600e77c032458b
Dovmial <nma91@mail.ru>
2025-04-24 19:15:06 +0500

    Added rotate

--- StarWarsConsoleApp/Adapters/MovableAdapter.cs
+++ StarWarsConsoleApp/Adapters/MovableAdapter.cs
@@ -11,6 +11,21 @@ namespace StarWarsConsoleApp.Adapters
             get => (Vector3)uObject.GetValue(nameof(IMovable.Position));
             set => uObject.SetValue(nameof(IMovable.Position), value);
         }
-        public Vector3 Velocity => (Vector3)uObject.GetValue(nameof(IMovable.Velocity));
+        public Vector3 Velocity
+        {
+            get
+            {
+                var velocity = (Vector3)uObject.GetValue(nameof(IMovable.Velocity));
+                if (uObject is IRotatable)
+                { 
+                    //radians
+                    var angle = (int)uObject.GetValue(nameof(IRotatable.RotationAngle));
+                    return new Vector3(
+                        (float)(velocity.X * Math.Cos(angle)),
+                        (float)(velocity.Y * Math.Sin(angle)), 0.0f);
+                }
+                return velocity;
+            }
+        }
     }
 }
